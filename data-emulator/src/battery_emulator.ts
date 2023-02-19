ce35e8b7460602188a166cf71f9b15de16159048
Alvin Cherk <alvin@cherk.org>
2023-02-20 10:20:44 +1100

    Add eslint + prettier and format

--- data-emulator/src/battery_emulator.ts
+++ data-emulator/src/battery_emulator.ts
@@ -1,16 +1,16 @@
-import net from 'net';
-import { exit } from 'process';
+import net from "net";
+import { exit } from "process";
 
 const tcpClient = new net.Socket();
-const HOST = "localhost"
-const PORT = 12000
+const HOST = "localhost";
+const PORT = 12000;
 
 const MILLISECONDS = 250;
 const ERROR_CHANCE = 15;
 
 function generate_and_send_battery_data() {
-    let generated_value: number = 0;
-    let error_flag = getRandomIntInclusive(1, ERROR_CHANCE);
+    let generated_value = 0;
+    const error_flag = getRandomIntInclusive(1, ERROR_CHANCE);
 
     switch (error_flag) {
         case 1:
@@ -24,23 +24,22 @@ function generate_and_send_battery_data() {
             break;
     }
 
-    let data = {
-        "battery_temperature": generated_value,
-        "timestamp": Date.now()
+    const data = {
+        battery_temperature: generated_value,
+        timestamp: Date.now(),
     };
 
     if (!(tcpClient.destroyed || tcpClient.closed)) {
-        let json_string = JSON.stringify(data)
+        let json_string = JSON.stringify(data);
         if (error_flag === 3) {
             // make invalid JSON string by adding an extra symbol
-            json_string += '}'
+            json_string += "}";
         }
         tcpClient.write(json_string);
     } else {
         console.log("connection to server closed");
         exit();
     }
-
 }
 
 function getRandomIntInclusive(min: number, max: number) {
@@ -50,14 +49,16 @@ function getRandomIntInclusive(min: number, max: number) {
 }
 
 tcpClient.connect(PORT, HOST, function () {
-    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    console.log("CONNECTED TO: " + HOST + ":" + PORT);
 });
 
-tcpClient.on('error', function (e) {
+tcpClient.on("error", function (e) {
     console.log(e.message);
 });
 
 tcpClient.on("connect", () => {
-    console.log(`starting to generate and send emulated battery data every ${MILLISECONDS} milliseconds`)
+    console.log(
+        `starting to generate and send emulated battery data every ${MILLISECONDS} milliseconds`,
+    );
     setInterval(generate_and_send_battery_data, MILLISECONDS);
-})
+});
