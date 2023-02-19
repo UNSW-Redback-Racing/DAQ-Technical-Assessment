616b9bb6f85e7d5dc39c441cc6c7c425f0007692
Alvin Cherk <alvin@cherk.org>
2023-02-20 10:04:42 +1100

    Part 2

--- streaming-service/src/server.ts
+++ streaming-service/src/server.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import net from 'net';
 import { WebSocket, WebSocketServer } from 'ws';
 
@@ -26,19 +27,81 @@ const isJSON = (item: string): boolean => {
     return false;
 }
 
+/**
+ * Checks if an object contains all keys in `fields`
+ * @param body request body
+ * @param fields fields to check
+ * @returns true if body contains all fields specified in `fields`
+ */
+export const isValidObject = <T extends Record<string, unknown>>(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    body: any,
+    fields: Array<keyof T>,
+): body is T => {
+    return Object.keys(body).every((key) => fields.includes(key));
+};
+
+type DataPacket = {
+    battery_temperature: number,
+    timestamp: number, // integer
+}
+
+let incidents: DataPacket[] = [];
+
 tcpServer.on('connection', (socket) => {
     console.log('TCP client connected');
-
     socket.on('data', (msg) => {
         console.log(msg.toString());
 
-        // HINT: what happens if the JSON in the received message is formatted incorrectly?
-        // HINT: see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+
+
         if (isJSON(msg.toString())) {
             const currJSON = JSON.parse(msg.toString());
+            if (isValidObject<DataPacket>(currJSON, ["battery_temperature", "timestamp"])) {
+                const dataPacket = currJSON;
+                if (dataPacket.battery_temperature > 80) {
+                    incidents.push(dataPacket);
+                }
+            } else {
+                console.error("Last message was missing keys!");
+            }
         } else {
             console.error("Last message was not JSON valid!");
         }
+        // console.log(incidents.length);
+        if (incidents.length >= 3) {
+            // Get the first 3
+            console.log("more than 3");
+            const incident1 = incidents[0]
+            const incident2 = incidents[1]
+            const incident3 = incidents[2]
+
+            if (Math.abs(incident3.battery_temperature - incident1.battery_temperature) < 5000) {
+                console.log(incident1, incident2, incident3);
+                // Current most timestamp should be incident3 (as in order)
+                fs.appendFile("incident.log", incident3.timestamp + '\n', (err) => {
+                    if (err) {
+                        console.error("Error writing to file, incident.log");
+                    }
+                })
+                incidents.pop();
+                incidents.pop();
+                incidents.pop();
+            }
+        }
+        // Purge incident if older than 5 seconds
+        if (incidents.length >= 1) {
+            const incident1 = incidents[0];
+            const timestamp = new Date().getTime();
+            // console.log(Math.abs(timestamp - incident1.timestamp));
+            if (Math.abs(timestamp - incident1.timestamp) >= 5000) {
+                const purge = incidents.pop() as DataPacket;
+                // console.log("purging after 5 seconds, " + (purge.timestamp - timestamp));
+            }
+        }
+
+
+
 
         websocketServer.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
