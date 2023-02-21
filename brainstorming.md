79355d2f390bacc12e00682a566c71a2f6489750
Vivian Cheng <vivianchengyy@gmail.com>
2023-02-21 17:12:17 +1100

    streaming_fix: Add catch error to avoid crash

--- brainstorming.md
+++ brainstorming.md
@@ -1,1 +1,4 @@
 # Brainstorming
+part 1: 
+crash happens when the msg has an incorrect format and is passed to JSON.parse, e.g. an extra '}'.
+So other than generating a syntax error and crash the whole service, try-and-catch is implemented to print an error message.
--- streaming-service/src/server.ts
+++ streaming-service/src/server.ts
@@ -14,7 +14,12 @@ tcpServer.on('connection', (socket) => {
 
         // HINT: what happens if the JSON in the received message is formatted incorrectly?
         // HINT: see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
-        let currJSON = JSON.parse(msg.toString());
+        try {
+            let currJSON = JSON.parse(msg.toString());  
+        } catch(error) {
+            console.error("Invalid Temperature Format");
+        }
+        
 
         websocketServer.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
