84c1671e542d21a9ea0c03727642cb16c88c58d6
Theo Ang <theo.ang816@gmail.com>
2023-02-28 17:06:21 +1100

    try add media query, layout change have ugly animation

--- battery-ui/src/Content.tsx
+++ battery-ui/src/Content.tsx
@@ -1,7 +1,6 @@
 import RedbackLogo from './RedbackLogo';
 import Temperature from './Temperature';
-import './App.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 interface TemperatureProps {
   temp: number;
@@ -10,29 +9,11 @@ interface TemperatureProps {
 const Content = ({ temp }: TemperatureProps) => {
 
   const [open, setOpen] = useState(false);
-  const [loadTemp, setLoadTemp] = useState(0);
-
-  useEffect(() => {
-    let timeout: NodeJS.Timeout;
-    console.log(open + " " + loadTemp);
-    if (loadTemp === 2 && open) {     // close
-      timeout = setTimeout(() => {
-        setOpen(false);
-        setLoadTemp(0);
-      }, 1000);
-    } else if (open) {                // open
-      timeout = setTimeout(() => setLoadTemp(1), 1000);
-    } 
-    return () => {
-      clearTimeout(timeout);
-    }
-  }, [open, loadTemp])
-  
 
   return (
     <>
-      <RedbackLogo open={open} setOpen={setOpen} setLoadTemp={setLoadTemp}/>
-      { loadTemp === 1 && <Temperature temp={temp}/> }
+      <RedbackLogo open={open} setOpen={setOpen}/>
+      <Temperature temp={temp} open={open}/>
     </>
   )
 }
--- battery-ui/src/Temperature.tsx
+++ battery-ui/src/Temperature.tsx
@@ -1,23 +1,54 @@
 import LiveValue from './live_value'
 import './App.css';
+import { motion } from 'framer-motion';
 import { useContext } from 'react';
 import { DimContext } from './DimensionsProvider';
 
 interface TemperatureProps {
   temp: number;
+  open: boolean;
 }
 
-const Temperature = ({ temp }: TemperatureProps) => {
+const Temperature = ({ temp, open }: TemperatureProps) => {
 
-  const {windowHeight, windowWidth, logoDimensions} = useContext(DimContext);
+  const { windowWidth } = useContext(DimContext); // could use media query but already have this
+  const MotionLiveValue = motion(LiveValue);  // custom framer motion component
+
+  const sideVariants = {
+    open: {
+			translateX: 0,
+      transition: { duration: 1, delay: 0.2 }
+		},
+    close: {
+			translateX: `50vw`,
+      transition: { duration: 1 }
+    },
+  }
+
+  const bottomVariants = {
+    open: {
+      translateX: `-50%`,
+      translateY: `-60vh`,
+      transition: { duration: 1, delay: 0.2 }
+		},
+    close: {
+      translateX: `-50%`,
+			translateY: 0,
+      transition: { duration: 1 }
+    },
+  }
 
   return (
-    <div className='temp-container'>
-      <p className='value-title'>
+    <motion.div className='temp-container'
+      animate={ open ? "open": "close" }
+      variants={ (windowWidth >= 796) ? sideVariants : bottomVariants }
+      initial={false}
+    >
+      <motion.p className='value-title'>
           Live Battery Temperature
-      </p>
-      <LiveValue temp={temp}/>
-    </div>
+      </motion.p>
+      <MotionLiveValue temp={temp}/>
+    </motion.div>
   )
 }
 
