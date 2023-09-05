56c3163ae1a2a05ae6618b94566b280acae04374
Edwin Ni <edwin@Edwins-MacBook-Pro.local>
2023-09-05 17:18:35 +1000

    Finished part 1 and 2

--- streaming-service/src/server.ts
+++ streaming-service/src/server.ts
@@ -1,46 +1,78 @@
-import net from 'net';
-import { WebSocket, WebSocketServer } from 'ws';
+import net from "net";
+import { WebSocket, WebSocketServer } from "ws";
+import fs from "fs";
 
-const TCP_PORT = parseInt(process.env.TCP_PORT || '12000', 10);
+const TCP_PORT = parseInt(process.env.TCP_PORT || "12000", 10);
 
 const tcpServer = net.createServer();
 const websocketServer = new WebSocketServer({ port: 8080 });
 
-tcpServer.on('connection', (socket) => {
-    console.log('TCP client connected');
-    
-    socket.on('data', (msg) => {
+const MAX_TEMP = 80;
+const MIN_TEMP = 20;
+
+let recentOverheats: number[] = [];
+
+function processTemperatureData(temperature: number, timestamp: number) {
+    if (temperature > MAX_TEMP || temperature < MIN_TEMP) {
+        recentOverheats.push(timestamp);
+
+        // Filter out timestamps older than 5 seconds
+        recentOverheats = recentOverheats.filter(
+            (timestamp) => Date.now() - timestamp <= 5000
+        );
+
+        console.log(recentOverheats.length);
+
+        if (recentOverheats.length > 3) {
+            logIncident(timestamp);
+        }
+    }
+}
+
+function logIncident(timestamp: number) {
+    const date = new Date(timestamp);
+    fs.appendFileSync("incidents.log", `Incident at: ${date.toISOString()}\n`);
+}
+
+tcpServer.on("connection", (socket) => {
+    console.log("TCP client connected");
+
+    socket.on("data", (msg) => {
         console.log(msg.toString());
 
-        // HINT: what happens if the JSON in the received message is formatted incorrectly?
-        // HINT: see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
-        let currJSON = JSON.parse(msg.toString());
+        try {
+            let currJSON = JSON.parse(msg.toString());
+            processTemperatureData(
+                currJSON.battery_temperature,
+                currJSON.timestamp
+            );
+        } catch (e) {
+            console.log(e);
+        }
 
         websocketServer.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
-              client.send(msg.toString());
+                client.send(msg.toString());
             }
-          });
+        });
     });
 
-    socket.on('end', () => {
-        console.log('Closing connection with the TCP client');
+    socket.on("end", () => {
+        console.log("Closing connection with the TCP client");
     });
-    
-    socket.on('error', (err) => {
-        console.log('TCP client error: ', err);
+
+    socket.on("error", (err) => {
+        console.log("TCP client error: ", err);
     });
 });
 
-websocketServer.on('listening', () => console.log('Websocket server started'));
+websocketServer.on("listening", () => console.log("Websocket server started"));
 
-websocketServer.on('connection', async (ws: WebSocket) => {
-    console.log('Frontend websocket client connected to websocket server');
-    ws.on('error', console.error);  
+websocketServer.on("connection", async (ws: WebSocket) => {
+    console.log("Frontend websocket client connected to websocket server");
+    ws.on("error", console.error);
 });
 
 tcpServer.listen(TCP_PORT, () => {
     console.log(`TCP server listening on port ${TCP_PORT}`);
 });
-
-
